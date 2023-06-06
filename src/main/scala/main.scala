58f878c7acc6e058e14545d7c57a7169f1fb759f
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-06-06 19:30:56 +0200

    Add a simple enum code example

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,5 @@
 import com.intellij.openapi.compiler.CompilerMessageCategory
+import com.intellij.compiler.ModuleSourceSet
 
 object main {
   def doSomething(category: CompilerMessageCategory): Unit = {
@@ -11,7 +12,18 @@ object main {
     println(messageType)
   }
 
+  def doSomethingSimple(moduleSourceType: ModuleSourceSet.Type): Unit = {
+    val something = moduleSourceType match {
+      case ModuleSourceSet.Type.TEST => "test"
+      case ModuleSourceSet.Type.RESOURCES => "res"
+      case ModuleSourceSet.Type.PRODUCTION => "prod"
+      case ModuleSourceSet.Type.RESOURCES_TEST => "res_test"
+    }
+    println(something)
+  }
+
   def main(args: Array[String]): Unit = {
     doSomething(CompilerMessageCategory.ERROR)
+    doSomethingSimple(ModuleSourceSet.Type.RESOURCES)
   }
 }
